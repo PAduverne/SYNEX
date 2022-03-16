6db9d98e01b2d8232e60db202beb10c00f886c68
baird <baird@apcmc2106.local>
2022-03-16 15:44:07 +0100

    Adding options to avoid savefiles that can cause memory problems if running on cluster.

--- SYNEX/SYNEX_Utils.py
+++ SYNEX/SYNEX_Utils.py
@@ -1723,26 +1723,27 @@ def TileSkyArea(source_or_kwargs,detectors=None,base_telescope_params=None,cloni
             detectors = None
             dict_list=None
             out_dirs=None
-        
+
         # Send source to workers
         source = comm.bcast(source, root=0)
 
         # Scatter detectors to processors -- can we formulate a trade off between processes per detectors and partitioning
         # pool to run gwemopt in parallel (doParallel and Ncores options in go_params)?
         # #### detectors = comm.scatter(detectors, root=0)
-        dict_list_ = comm.scatter(dict_list, root=0)
-        out_dirs_ = comm.scatter(out_dirs, root=0)
-        if not isinstance(dict_list,list): dict_list_=[dict_list_] # in case broadcast gives one of each to processors
+        dict_list = comm.scatter(dict_list, root=0)
+        out_dirs = comm.scatter(out_dirs, root=0)
+        if not isinstance(dict_list,list): dict_list=[dict_list] # in case broadcast gives one of each to processors
+        if not isinstance(out_dirs,list): out_dirs=[out_dirs] # in case broadcast gives one of each to processors
 
         # Checks
-        print("MPI rank/size: %d / %d" % (MPI_rank, MPI_size),"with 'detectors' shape:",np.shape(detectors), flush=True)
+        print("MPI rank/size: %d / %d" % (MPI_rank, MPI_size),"with 'dict_list' shape:",np.shape(dict_list), flush=True)
 
     # Make detectors list and finish source EM calculations based on broadcast detectors only if ARF files does not change in detectors
-    detectors = [SYDs.Athena(**dict_ii) for dict_ii in dict_list_]
+    detectors = [SYDs.Athena(**dict_ii) for dict_ii in dict_list]
     if len(set([detector.ARF_file_loc_name for detector in detectors]))==1: source.GenerateCTR(detectors[0].ARF_file_loc_name,gamma=1.7)
 
     # Loop over list of lists if we need to -- if we cloned more than one param
-    for i in range(len(detectors)): go_params, map_struct, tile_structs, coverage_struct, detectors[i] = TileWithGwemopt(source,detectors[i],out_dirs_[i])
+    for i in range(len(detectors)): go_params, map_struct, tile_structs, coverage_struct, detectors[i] = TileWithGwemopt(source,detectors[i],out_dirs[i])
 
     return detectors
 

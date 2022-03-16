1a25d63fdc73730ba9b12655eea35e0a15da93fa
baird <baird@apcmc2106.local>
2022-03-16 15:06:27 +0100

    Adding options to avoid savefiles that can cause memory problems if running on cluster.

--- SYNEX/SYNEX_Utils.py
+++ SYNEX/SYNEX_Utils.py
@@ -1721,6 +1721,7 @@ def TileSkyArea(source_or_kwargs,detectors=None,base_telescope_params=None,cloni
         if MPI_rank>0:
             source = None
             detectors = None
+            dict_list=None
 
         # Send source to workers
         source = comm.bcast(source, root=0)
